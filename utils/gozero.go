e5ff0a10ce15d998a6013639f48fd214ed097ac7
lijianjunljj <491671642@qq.com>
2023-06-27 23:57:16 +0800

    l

--- utils/gozero.go
+++ utils/gozero.go
@@ -5,18 +5,30 @@ import (
 	"github.com/zeromicro/go-zero/core/discov"
 	"fmt"
 )
+func ZrpcConnDirect(dsn string) zrpc.Client  {
+	conn := zrpc.MustNewClient(zrpc.RpcClientConf{
+		Target: dsn,
+		NonBlock: true,
+	})
+	return conn
+}
+
 func ZrpcConn(serviceName string,option ...interface{}) zrpc.Client  {
-	var dsn string
+	var dsn,user,pass string
 	if len(option) > 0  {
 		dsn = option[0].(string)
+		user = option[1].(string)
+		pass = option[2].(string)
 	}else{
 		dsn = "127.0.0.1:2379"
 	}
-	fmt.Println("dsn:",dsn)
+	fmt.Println("dsn,user,pass:",dsn,user,pass)
 	conn := zrpc.MustNewClient(zrpc.RpcClientConf{
 		Etcd: discov.EtcdConf{ // 通过 etcd 服务发现时，只需要给 Etcd 配置即可
 			Hosts: []string{dsn},
 			Key:   serviceName,
+			User: user,
+			Pass: pass,
 		},
 		NonBlock: true,
 	})
